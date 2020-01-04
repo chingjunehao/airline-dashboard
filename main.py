02db543b1dd778c356af8c6ad67ff91e91484e9c
chingjunehao <junehao0722@hotmail.com>
2020-01-04 17:05:59 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+
+application = Flask(__name__, template_folder="light-bootstrap-dashboard-master/BS3", static_folder="light-bootstrap-dashboard-master/BS3/assets")
+
+@application.route('/')
+def dashboard():
+	return render_template('dashboard.html')
+
+if __name__ == '__main__':
+	application.run(debug=True)
