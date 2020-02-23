530e4d7fdd434a452c85a575c3b46d6bdab5f6f4
Too Seng Wei <sengweitoo@gmail.com>
2020-02-24 02:10:24 +0800

    added user sentiment and social media analysis

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ application = Flask(__name__, template_folder="light-bootstrap-dashboard-master/
 def dashboard():
 	return render_template('campagin.html')
 
+@application.route('/campaign')
+def campaign():
+	return render_template('campagin.html')
+
+@application.route('/sentiment')
+def sentiment():
+	return render_template('sentiment.html')
+
+@application.route('/sna')
+def sna():
+	return render_template('sna.html')
+
 if __name__ == '__main__':
 	application.run(debug=True, port=5001)
